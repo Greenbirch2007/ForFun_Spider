57c39a5fbcd8fc71cf1606dd8e8e2aad109f3c00
Greenbirch2007 <291109028@qq.com>
2019-12-20 14:44:58 +0800

    self代表类的实例，而非类。

--- 好看视频/HaoKan_details.py
+++ 好看视频/HaoKan_details.py
@@ -43,7 +43,7 @@ from GDG_link import f_l
 # -----------------重构代码　1:写一个类，用类中的方法互相调用  2. 用with open上下文来下载-
 
 
-
+# self
 class Gdgvedio(object):
     def __init__(self,url):# 初始化时，直接传入url,相当于过渡了一层
         self.url =url
@@ -52,34 +52,32 @@ class Gdgvedio(object):
         try:
             response = requests.get(self.url)
             if response.status_code == 200:
-                self.response = response.text
-                return self.response
+                s_response = response.text
+                return s_response
             return None
 
         except RequestException:
             return None
 
     def parse_Video(self):
+        s_html = self.get_one_page()
         pattern = re.compile('<video class="video" src=(.*?)></video>'+'.*?<h2 class="videoinfo-title">(.*?)</h2>',re.S)
-        items = re.findall(pattern,self.response)
+
+        items = re.findall(pattern,s_html)
         for item in items:
             n_path = os.getcwd()
 
             # 　使用request.urlretrieve 来下载
-            # urllib.request.urlretrieve(item[0], '{0}/{1}.mp4'.format(n_path,item[1]))
-            #　使用with open来下载
-            with open('{0}/{1}.mp4'.format(n_path,item[1]), 'wb') as f:  # 图片不需要encoding编码
-                f.write(self.response.content)  # 写入字节流来保存文件
-                print("下载成功！")
+            urllib.request.urlretrieve(item[0], '{0}/{1}.mp4'.format(n_path,item[1]))
 
 
 
 
 
 
-if __name__ == '__main__':
-    url = 'www.baidu.com'
-    v = Gdgvedio(url)
-    v.parse_Video()
 
-
+if __name__ =="__main__":
+    for url_n in f_l:
+        f_url = 'https://haokan.baidu.com/v?vid={0}'.format(url_n)
+        v = Gdgvedio(f_url)
+        v.parse_Video()
